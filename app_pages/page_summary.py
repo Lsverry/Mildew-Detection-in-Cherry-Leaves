2de2277a49d2c0941d758fcc06236ad4d308651a
Luis Echeverry <nmx0040@gmail.com>
2024-09-28 12:43:06 +0000

    Removed unused predictive_analysis file, restored load_model_or_data function, ensure code complies with PEP8 standard.

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -2,8 +2,13 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 def page_summary_body():
+    """
+    Displays a quick summary of the project, including general information, dataset overview,
+    and project objectives.
+    """
     st.write("### Quick Project Summary")
 
+    # General information about the project
     st.info(
         "**General Information**\n"
         "* Powdery mildew is a fungal disease that affects various plants, "
@@ -12,18 +17,21 @@ def page_summary_body():
         "* By leveraging machine learning, we automate the detection process to help farmers identify infected plants earlier, reducing potential crop damage.\n"
     )
 
+    # Overview of the project dataset
     st.write(
         "**Project Dataset**\n"
         "* The available dataset contains over 4,000 images of cherry leaves, categorized into 'healthy' and 'powdery mildew infected'.\n"
         "* The images are used to train a deep learning model to classify the condition of the leaves based on visual features.\n"
     )
 
+    # Link to the project's README
     st.write(
         f"For additional information, please refer to the [**README**](https://github.com/Lsverry/Mildew-Detection-in-Cherry-Leaves/tree/main) in the project repository."
     )
 
+    # Display the project objectives
     st.success(
-    "### Project Objectives:\n"
-    "1. **Visual Study**: Provide a visual study to help differentiate between healthy and mildew-infected cherry leaves.\n"
-    "2. **Prediction Model**: Build a model to predict whether a given leaf image is healthy or infected."
+        "### Project Objectives:\n"
+        "1. **Visual Study**: Provide a visual study to help differentiate between healthy and mildew-infected cherry leaves.\n"
+        "2. **Prediction Model**: Build a model to predict whether a given leaf image is healthy or infected."
     )
